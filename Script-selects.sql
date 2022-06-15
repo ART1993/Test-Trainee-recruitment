88a93a967681601ea26ae388eba762349a7b9eb5
ART1993 <103036596+ART1993@users.noreply.github.com>
2022-06-15 11:50:16 +0200

    Add files via upload

--- Script-selects.sql
+++ Script-selects.sql
@@ -1,7 +1,8 @@
-select * from performance;
-select * from visits_data;
+--select * from performance;
+--select * from visits_data;
 --select * from leads;
 --select * from sales;
+
 select performance.date_d, clicks , cost_add from performance
 left join visits_data
 on performance.date_d=visits_data.date_d;
@@ -13,9 +14,15 @@ select performance.date_d  as dates, performance.channel as channel,
 	   on performance.date_d=leads.date_d
 	   right join sales
 	   on sales.date_d=performance.date_d
+	   where performance.date_d is not null
 	   group by dates, channel, ad_ID, keyword;
 	   
-select SUM(performance.cost_add) as costs,AVG(performance.cost_add) as costs, leads.uuid as UUID 
+select SUM(performance.cost_add) as total_cost,AVG(performance.cost_add) as average_cost, leads.uuid as UUID 
 from performance right join leads
 on performance.date_d =leads.date_d 
+group by UUID
+
+select SUM(performance.cost_add) as total_cost,AVG(performance.cost_add) as average_cost, sales.product_id  as UUID 
+from performance right join sales
+on performance.date_d =sales.date_d 
 group by UUID
